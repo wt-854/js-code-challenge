c12624482fc108a4a9fd6712c79182c8d701f3e8
wt854 <wt854@outlook.com>
2020-11-17 20:42:13 +0800

    updated problem1 solution c

--- src/three_ways_to_sum_to_n/solution_c.js
+++ src/three_ways_to_sum_to_n/solution_c.js
@@ -3,10 +3,27 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
-var sum_to_n = function (n) {
-  // recursive
-  if (n <= 1) {
-    return n;
-  }
-  return n + sum_to_n(n - 1);
+const memoize = (fn) => {
+  let cache = {};
+  return (...args) => {
+    let n = args[0];
+    if (n in cache) {
+      // console.log('Fetching from cache', n);
+      return cache[n];
+    } else {
+      // console.log('Calculating result', n);
+      let result = fn(n);
+      cache[n] = result;
+      return result;
+    }
+  };
 };
+
+const sum_to_n = memoize((x) => {
+  // recursive + memoization
+  if (x === 0) {
+    return 0;
+  } else {
+    return x + sum_to_n(x - 1);
+  }
+});
